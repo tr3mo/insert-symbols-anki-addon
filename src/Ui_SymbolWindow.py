46a172f4e493dc44264062ac0f8b61ef824ae5b5
tr3mo <75224367+tr3mo@users.noreply.github.com>
2023-12-24 14:01:44 +0100

    Update Ui_SymbolWindow.py

--- src/Ui_SymbolWindow.py
+++ src/Ui_SymbolWindow.py
@@ -2,11 +2,11 @@
 
 # Form implementation generated from reading ui file 'Ui_SymbolWindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 6
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 class Ui_SymbolWindow(object):
     def setupUi(self, SymbolWindow):
